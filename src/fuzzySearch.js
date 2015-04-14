6f0baad03cb406c95e68b72d3c5b36f1bcc87734
Drew Cuthbertson <syeoryn@gmail.com>
2015-04-13 22:32:59 -0700

    Implement fuzzySearch and Levenshtein solution

--- src/fuzzySearch.js
+++ src/fuzzySearch.js
@@ -1,4 +1,33 @@
 var fuzzySearch = function(input, dictionary){
-  /* implement me! */
+  var result;
+  if(input in dictionary){
+    result = input;
+  } else {
+    var bestDistance = Infinity;
+    var bestRelevance = Infinity;
+    var newBest = false;
+    var distance;
+    var relevance;
+
+    for(var word in dictionary){
+      distance = levenshteinDistance(word, input);
+      relevance = dictionary[word];
+
+      if(distance < bestDistance){
+        newBest = true;
+      } else if(distance === bestDistance && relevance < bestRelevance){
+        newBest = true;
+      }
+
+      if(newBest){
+        result = word;
+        bestDistance = distance;
+        bestRelevance = relevance;
+        newBest = false;
+      }
+    }
+  }
+
+  return result
 }
 
--- src/levenshtein.js
+++ src/levenshtein.js
@@ -1,4 +1,27 @@
 var levenshteinDistance = function(word1, word2){
-  /* Implement me! */
+  var grid = [];
+
+  for(var i = 0; i <= word1.length; i++){
+    grid.push([i]);
+  }
+
+  for(var j = 1; j <= word2.length; j++){
+    grid[0].push(j);
+  }
+
+  for(var i = 1; i <= word1.length; i++){
+    for(var j = 1; j <= word2.length; j++){
+      if(word1[i - 1] === word2[j - 1]){
+        grid[i][j] = grid[i - 1][j - 1];
+      } else {
+        var above = grid[i - 1][j];
+        var left = grid[i][j - 1];
+        var aboveLeft = grid[i - 1][j - 1];
+        grid[i][j] = Math.min(above, left, aboveLeft) + 1;
+      }
+    }
+  }
+
+  return grid[word1.length][word2.length];
 }
 
